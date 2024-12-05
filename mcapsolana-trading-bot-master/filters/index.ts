1102eb97e2c878f6d6b5ba686e1d7f27b22dff8d
Rowaid-x <105711722+Rowaid-x@users.noreply.github.com>
2024-12-05 21:17:04 +0300

    Delete mcapsolana-trading-bot-master directory

--- mcapsolana-trading-bot-master/filters/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './burn.filter';
-export * from './mutable.filter';
-export * from './pool-filters';
-export * from './pool-size.filter';
-export * from './renounced.filter';
-// Export filters
-
-export * from './market-cap.filter';
